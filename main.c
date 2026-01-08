505d09e72b2cfdedacce87d08346e65bfad7fced
karolkwiatek1 <karolbkwiatek@gmail.com>
2026-01-08 21:18:41 +0100

    basic optimization by storing a state in 1d array and sending data to render all at once

--- main.c
+++ main.c
@@ -1,117 +1,163 @@
 #include "raylib.h"
+#include <stdlib.h>
 
 // config
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
-#define CELL_SIZE 4
+#define CELL_SIZE 1
 
 // grid size based on cell size
 #define COLS (SCREEN_WIDTH / CELL_SIZE)
 #define ROWS (SCREEN_HEIGHT / CELL_SIZE)
 
 // 0 = empty, 1 = sand
-int grid[COLS][ROWS] = {0};
+char *grid;
 int grainCount = 0;
+unsigned int brushSize = 50;
+unsigned int physicsSteps = 1;
+
+void addGrains() {
+    Vector2 mouse = GetMousePosition();
+
+    int mx = (int)mouse.x / CELL_SIZE;
+    int my = (int)mouse.y / CELL_SIZE;
+
+    int r = brushSize / CELL_SIZE;
+    if (r < 1) r = 1;
+
+    for (int y = -r; y <= r; y++){
+        for (int x = -r; x <= r; x++){
+            if (x*x + y*y <= r*r) {
+                int dx = mx + x;
+                int dy = my + y;
+                if (dx >= 0 && dx < COLS && dy >= 0 && dy < ROWS) {
+                    //index in 1d array = y * width + x;
+                    int idx = dy * COLS + dx;
+                    if (grid[idx] == 0) {
+                        grid[idx] = 1;
+                        grainCount++;
+                    }
+                }
+            }
+        }
+    }
+}
+
+void removeGraind() {
+    Vector2 mouse = GetMousePosition();
+
+    int mx = (int)mouse.x / CELL_SIZE;
+    int my = (int)mouse.y / CELL_SIZE;
+
+    int r = brushSize / CELL_SIZE;
+    if (r< 1) r = 1;
+
+    for (int y = -r; y <= r; y++){
+        for (int x = -r; x <= r; x++){
+            if (x*x + y*y <= r*r) {
+                int dx = mx + x;
+                int dy = my + y;
+                if (dx >= 0 && dx < COLS && dy >= 0 && dy < ROWS) {
+                    //index in 1d array = y * width + x;
+                    int idx = dy * COLS + dx;
+                    if (grid[idx] == 1) {
+                        grid[idx] = 0;
+                        grainCount--;
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 int main(void) {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sand simulator");
-    SetTargetFPS(120);
+    SetTargetFPS(60);
+
+    //allocating memory for the grid (using flat array for performance)
+    grid = (char *)calloc(COLS * ROWS, sizeof(char));
+
+    Image sandImage = GenImageColor(COLS, ROWS, BLACK);
+    Texture2D sandTexture = LoadTextureFromImage(sandImage);
+
+    Color *pixels = (Color *)sandImage.data;
 
     while (!WindowShouldClose()) {
+
+        if (IsKeyPressed(KEY_UP)) physicsSteps++;
+
+        if (IsKeyPressed(KEY_DOWN) && physicsSteps > 1) physicsSteps--;
+
         if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
-            Vector2 mouse = GetMousePosition();
-            
-            int mouseGridX = (int)mouse.x / CELL_SIZE;
-            int mouseGridY = (int)mouse.y / CELL_SIZE;
-            
-            int radiusInGrid = 50 / CELL_SIZE; 
-            if (radiusInGrid < 1) radiusInGrid = 1;
-
-            for (int y = -radiusInGrid; y <= radiusInGrid; y++) {
-                for (int x = -radiusInGrid; x <= radiusInGrid; x++) {
-                    
-                    if (x*x + y*y <= radiusInGrid*radiusInGrid) {
-                        int drawX = mouseGridX + x;
-                        int drawY = mouseGridY + y;
-
-                        if (drawX >= 0 && drawX < COLS && drawY >= 0 && drawY < ROWS) {
-                            // checking if pixel is empty for grain counter
-                            if (grid[drawX][drawY] == 0){
-                                grid[drawX][drawY] = 1;
-                                grainCount++;
-                            }
-                        }
-                    }
-                }
-            }
+            addGrains();
         }
         
         if (IsMouseButtonDown(MOUSE_RIGHT_BUTTON)) {
-            Vector2 mouse = GetMousePosition();
-            
-            int mouseGridX = (int)mouse.x / CELL_SIZE;
-            int mouseGridY = (int)mouse.y / CELL_SIZE;
-            
-            int radiusInGrid = 50 / CELL_SIZE; 
-            if (radiusInGrid < 1) radiusInGrid = 1;
+            removeGraind();
+        }
 
-            for (int y = -radiusInGrid; y <= radiusInGrid; y++) {
-                for (int x = -radiusInGrid; x <= radiusInGrid; x++) {
-                    
-                    if (x*x + y*y <= radiusInGrid*radiusInGrid) {
-                        int drawX = mouseGridX + x;
-                        int drawY = mouseGridY + y;
+        //UPDATE (cpu)
+        for (int i = 0; i < physicsSteps; i++) {
+            for (int y = ROWS - 2; y >= 0; y--) {
+                for (int x = 0; x < COLS; x ++) {
+                    int idx = y * COLS + x;
 
-                        if (drawX >= 0 && drawX < COLS && drawY >= 0 && drawY < ROWS) {
-                            //checking if pixel is full for grain counter
-                            if (grid[drawX][drawY] == 1){
-                                grid[drawX][drawY] = 0;
-                                grainCount--;
-                            }
+                    if (grid[idx] == 1) {
+                        int below = (y + 1) * COLS + x;
+                        int belowLeft = (y + 1) * COLS + (x - 1);
+                        int belowRight = (y + 1) * COLS + (x + 1);
+
+                        // 1. below
+                        if (grid[below] == 0) {
+                            grid[below] = 1;
+                            grid[idx] = 0;
+                        }
+                        
+                        // 2. below-left
+                        else if (x > 0 && grid[belowLeft] == 0) {
+                            grid[belowLeft] = 1;
+                            grid[idx] = 0;
+                        }
+
+                        // 2. below-right
+                        else if (x < COLS -1 && grid[belowRight] == 0) {
+                            grid[belowRight] = 1;
+                            grid[idx] = 0;
                         }
                     }
                 }
             }
         }
 
-        for (int y = ROWS - 2; y >= 0; y--) {
-            for (int x = 0; x < COLS; x++) {
-                
-                if (grid[x][y] == 1) { 
-                    
-                    if (grid[x][y+1] == 0) {
-                        grid[x][y+1] = 1;
-                        grid[x][y] = 0;
-                    } 
-                    else if (x > 0 && grid[x-1][y+1] == 0) {
-                        grid[x-1][y+1] = 1;
-                        grid[x][y] = 0;
-                    }
-                    else if (x < COLS - 1 && grid[x+1][y+1] == 0) {
-                        grid[x+1][y+1] = 1;
-                        grid[x][y] = 0;
-                    }
-                }
+        //RENDER PREPARATION
+        for (int i = 0; i < COLS * ROWS; i++) {
+            if (grid[i] == 1) {
+                pixels[i] = YELLOW;
+            } else {
+                pixels[i] = BLACK;
             }
         }
 
+        UpdateTexture(sandTexture, pixels);
+
+        //DRAW
         BeginDrawing();
         ClearBackground(BLACK);
 
-        for (int y = 0; y < ROWS; y++) {
-            for (int x = 0; x < COLS; x++) {
-                if (grid[x][y] == 1) {
-                    DrawRectangle(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, YELLOW);
-                }
-            }
-        }
+        DrawTexturePro(sandTexture, (Rectangle){0, 0, COLS, ROWS}, (Rectangle){0, 0, SCREEN_WIDTH, SCREEN_HEIGHT}, (Vector2){0, 0}, 0.0f, WHITE);
 
         DrawFPS(10, 10);
-        
+        DrawText(TextFormat("Physics steps per frame: %d, simulated FPS: %d", physicsSteps, GetFPS() * physicsSteps), 10, 40, 20, GREEN);
         DrawText(TextFormat("Grains: %d", grainCount), SCREEN_WIDTH - 220, 10, 20, GREEN);
         
         EndDrawing();
     }
+
+    //CLEANING
+    UnloadImage(sandImage);
+    UnloadTexture(sandTexture);
+    free(grid);
 
     CloseWindow();
     return 0;
